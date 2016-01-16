ceb3a1eb2901663acc278cde79099efe0bcebe60
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-16 17:57:46 +0000

    Add handleProduct. Fix incorrect use of int32 for json key

--- product/main.go
+++ product/main.go
@@ -4,9 +4,11 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
+	"encoding/json"
+	"fmt"
 )
 
-var products map[int32]Product
+var products map[string]Product
 
 func init() {
 	products = createProducts()
@@ -22,22 +24,29 @@ type Product struct {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/products/{productId}", handleAllProducts)
+	router.HandleFunc("/products", handleAllProducts)
 	router.HandleFunc("/products/{productId}", handleProduct)
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
 
 func handleAllProducts(w http.ResponseWriter, r *http.Request) {
-
+	b, err := json.Marshal(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+	}
+	w.Write(b)
 }
 
 func handleProduct(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func createProducts() map[int32]Product {
-	return map[int32]Product{
-		1: Product{"1", "12345", "Widget", "Premium Widget", 120},
+func createProducts() map[string]Product {
+	return map[string]Product{
+		"1": Product{"1", "12345", "Widget", "Premium Widget", 120},
+		"2": Product{"2", "56789", "Splunket", "Basic Splunket", 20},
+		"3": Product{"3", "45678", "Bolt", "Premium Bolt", 260},
 	}
 }
 
