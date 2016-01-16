1b4ca6042bb869146ff9c6cad3fbf22e86d8ba2e
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-16 17:43:56 +0000

    First pass at product service skeleton

--- product/main.go
+++ product/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+)
+
+var products map[int32]Product
+
+func init() {
+	products = createProducts()
+}
+
+type Product struct {
+	Id          string `json:"id"`
+	Sku         string `json:"sku"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int64 `json:"price"`
+}
 
 func main() {
-	fmt.Println("product")
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{productId}", handleAllProducts)
+	router.HandleFunc("/products/{productId}", handleProduct)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
+
+func handleAllProducts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleProduct(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createProducts() map[int32]Product {
+	return map[int32]Product{
+		1: Product{"1", "12345", "Widget", "Premium Widget", 120},
+	}
+}
+
