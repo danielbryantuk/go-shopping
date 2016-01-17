03fa822a5a79f5eaee1c39b153fc43ed1a722d35
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-17 11:47:24 +0000

    Convert store to call product service for data

--- store/main.go
+++ store/main.go
@@ -4,33 +4,33 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
-	"fmt"
 	"html/template"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 const viewDir = "views/"
 
-var sampleProduct Product
-
-func init() {
-	sampleProduct = Product{"1", "12345", "Widget", "Premium brand widgets", 120}
-}
+//service locations - this would obviously be read from an external source
+const storeService = "localhost:3000"
+const productService = "http://localhost:3010/"
 
 type Product struct {
-	Id          string
-	Sku         string
-	Name        string
-	Description string
-	Price       int64
+	Id          string `json:"id"`
+	Sku         string `json:"sku"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int64 `json:"price"`
 }
 
 func main() {
-	//TODO - Do we need Gorilla for this example??
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/product", productHandler)
 	r.HandleFunc("/view/product", productViewHandler)
-	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Fatal(http.ListenAndServe(storeService, r))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,14 +38,31 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v", sampleProduct)
+	//todo
 }
 
 func productViewHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(viewDir + "shopFront.html")
+	resp, err := http.Get(productService + "products")
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var products map[string]Product
+
+	if err := json.Unmarshal(body, &products); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(os.Stderr, err)
 	} else {
-		t.Execute(w, sampleProduct)
+		t, err := template.ParseFiles(viewDir + "shopFront.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(os.Stderr, err)
+		} else {
+			t.Execute(w, products)
+		}
 	}
 }
