31ee23fc5c98d612d992ee0fbb11f046fea4781a
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-19 17:43:00 +0000

    Add basic basket service WIP

--- basket/main.go
+++ basket/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+type Basket struct {
+	UserId   string `json:"userId"`
+	products map[string]int32 `json:products`
+}
+
+//todo - is int32 appropriate here?
+var basketStore = make(map[string]map[string]int32)
 
 func main() {
-	fmt.Println("basket")
+	r := mux.NewRouter()
+	r.HandleFunc("/", healthHandler)
+	r.HandleFunc("basket/{userId}", basketHandler)
+	log.Fatal(http.ListenAndServe(":3020", r))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func basketHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	basket, ok := basketStore[userId]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		b, err := json.Marshal(basket)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Write(b)
+	}
 }
