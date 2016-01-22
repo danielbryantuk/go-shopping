d797d06fa082f22f69b885469b4ac27fe935b4bb
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-22 19:42:49 +0000

    Add new test for adding to basket twice

--- basket/main_test.go
+++ basket/main_test.go
@@ -6,6 +6,9 @@ import (
 )
 
 func TestUpdateBasket(t *testing.T) {
+	//todo - find better way of resetting package-level vars between tests
+	basketStore = make(map[string]Basket)
+
 	updateBasket("1", "2", 3)
 	expected := Basket{"1", map[string]int{"2":3}}
 	actual := basketStore["1"]
@@ -14,3 +17,18 @@ func TestUpdateBasket(t *testing.T) {
 		t.Errorf("%v != v%", actual, expected)
 	}
 }
+
+func TestUpdateBasketTwice(t *testing.T) {
+	//todo - find better way of resetting package-level vars between tests
+	basketStore = make(map[string]Basket)
+
+	updateBasket("1", "2", 3)
+	updateBasket("1", "2", 2)
+	expected := Basket{"1", map[string]int{"2":5}}
+	actual := basketStore["1"]
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("%v != v%", actual, expected)
+	}
+}
+
