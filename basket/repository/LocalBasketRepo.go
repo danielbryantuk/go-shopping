85eef89f8c70e4a41fad9f27b7b50779819aad17
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-24 11:12:10 +0000

    Add new repo files

---
+++ basket/repository/LocalBasketRepo.go
@@ -0,0 +1,40 @@
+package repository
+
+
+//userId, Basket
+var basketStore = make(map[string]Basket)
+
+func UpdateBasket(userId string, productId string, quantity int) {
+	//todo return bool indicating success
+	if basket, ok := basketStore[userId]; ok {
+		//we have a basket for the user
+		//update content
+		if _, ok := basket.Products[productId]; ok {
+			//bump count
+			basket.Products[productId] = basket.Products[productId] + quantity
+		} else {
+			//add one more
+			basket.Products[productId] = quantity
+			basketStore[userId] = basket
+		}
+
+	} else {
+		var products = make(map[string]int)
+		products[productId] = quantity
+		basket = Basket{userId, products}
+		basketStore[userId] = basket
+	}
+}
+
+func GetStoreAsMap() (map[string]Basket) {
+	return basketStore
+}
+
+func GetBasket(userId string) (Basket, bool) {
+	//todo - I don't think this is necessary - just return map access?
+	if basket, ok := basketStore[userId]; ok {
+		return basket, true
+	} else {
+		return nil, false
+	}
+}
---
+++ basket/repository/basketRepo.go
@@ -0,0 +1,12 @@
+package repository
+
+type Basket struct {
+	UserId   string `json:"userId"`
+	Products map[string]int `json:"products"`
+}
+
+type BasketRepo interface {
+	UpdateBasket(userId string, productId string, quantity int)
+	GetStoreAsMap() (map[string]Basket)
+	GetBasket(userId string) (Basket, bool)
+}
